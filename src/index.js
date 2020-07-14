c03c27e96613449750c0c98a115e6e3d95bd694c
bbtrombone <bbtrombone@sbcglobal.net>
2020-07-14 15:45:27 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
+
+
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  // your code 
+  let form = document.querySelector('form')
+const handleSubmit = function(ele){
+  ele.preventDefault()
+  console.log(ele.target.newTaskDescription.value)
+  newLi(ele.target.newTaskDescription.value)
+  // ele.target.reset();
+}
+form.addEventListener("submit",handleSubmit)
+//  const buildtoDo = function(toDo) {
+//    let li = document.createElement('li')
+//    li.innerText = toDo
+//  }
+
+ const newLi = function(item){
+  let li = document.createElement('li')
+  li.innerText= item
+  toDoUL.appendChild(li)
+ }
+
+  let toDoUL = document.querySelector('ul')
+
+// document.appendChild(toDo)
 });
+
+
+
